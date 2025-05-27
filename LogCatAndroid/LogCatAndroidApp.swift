3d746535463d8c734fbf169c5627b86c03e896c9
mikhail <v812iper@gmail.com>
2025-05-27 12:07:48 +0200

    Initial Commit

---
+++ LogCatAndroid/LogCatAndroidApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogCatAndroidApp.swift
+//  LogCatAndroid
+//
+//  Created by Mikhail on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LogCatAndroidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LogCatAndroidTests/LogCatAndroidTests.swift
@@ -0,0 +1,16 @@
+//
+//  LogCatAndroidTests.swift
+//  LogCatAndroidTests
+//
+//  Created by Mikhail on 27/05/2025.
+//
+
+import Testing
+
+struct LogCatAndroidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
